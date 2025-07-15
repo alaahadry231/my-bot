b096735c62fc18e5748d6a0f8a5ced9d69401c26
alaahadry231 <alloushhaadry@gmail.com>
2025-07-15 03:45:44 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,11 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "أهلاً! أنا بوت شغال على Render!")
+
+bot.polling()
